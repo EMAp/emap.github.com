a4715028fbb2dbbf57ce30484f749eb448a5d7ec
Alexandre Rademaker <arademaker@gmail.com>
2011-11-29 17:18:08 -0200

    fix filter

--- _plugins/summarize.rb
+++ _plugins/summarize.rb
@@ -1,7 +1,10 @@
 module Jekyll
-  module Filters
+  module AssetFilter
     def summarize(str, splitstr = /\s*,/)
       str.split(splitstr)[0]
     end
   end
 end
+
+Liquid::Template.register_filter(Jekyll::AssetFilter)
+
